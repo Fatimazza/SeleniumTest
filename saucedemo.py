3fd0483e11e9b537d18f8aa5e66f98922674a7b3
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-12-14 18:59:37 +0700

    Selenium Python Saucedemo - Add Unit Test to add a Product to Cart successfully

--- saucedemo.py
+++ saucedemo.py
@@ -75,6 +75,34 @@ class TestLogin(unittest.TestCase):
         self.assertEqual(product_price, '$15.99')
         self.assertIn('data:image/png;base64', product_image)
 
+    def test_d_success_add_product_to_cart(self):
+        # step to open browser
+        browser = self.browser
+        browser.get("https://www.saucedemo.com/")
+        browser.maximize_window()
+        time.sleep(5)
+        # step to fill in email and password
+        browser.find_element(By.ID,"user-name").send_keys("standard_user") 
+        browser.find_element(By.ID,"password").send_keys("secret_sauce") 
+        browser.find_element(By.XPATH,"/html//input[@id='login-button']").click()
+        time.sleep(5)
+        # assert response message
+        product_3_xpath = "/html//div[@id='inventory_container']/div/div[@id='inventory_container']/div/div[3]/div[@class='inventory_item_description']/div[@class='inventory_item_label']/a[@href='#']/div[@class='inventory_item_name']"
+        response_message = browser.find_element(
+            By.XPATH,product_3_xpath).text
+        self.assertEqual(response_message, 'Sauce Labs Bolt T-Shirt')
+        # step to add product to cart
+        browser.find_element(By.ID,"add-to-cart-sauce-labs-bolt-t-shirt").click()
+        browser.find_element(By.ID,"shopping_cart_container").click()
+        time.sleep(3)
+        # assert response message
+        product_name = browser.find_element(
+            By.CSS_SELECTOR,"div.inventory_item_name").text
+        product_qty = browser.find_element(
+            By.CSS_SELECTOR,"div.cart_quantity").text
+        self.assertEqual(product_name, 'Sauce Labs Bolt T-Shirt')
+        self.assertEqual(product_qty, '1')
+
     def test_e_success_logout(self):
         # step to open browser
         browser = self.browser
@@ -95,7 +123,6 @@ class TestLogin(unittest.TestCase):
         response_message = browser.find_element(
             By.XPATH,"/html//input[@id='login-button']").get_attribute("value")
         self.assertEquals(response_message, "Login")
-
 
     def tearDown(self):
         self.browser.close()
