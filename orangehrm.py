21e81b3d12521bb1cfd6644413a0a11d4a23102c
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-12-14 13:54:09 +0700

    Selenium Python Orangehrm - Add Unit Test to Login successfully

--- orangehrm.py
+++ orangehrm.py
@@ -9,10 +9,25 @@ class TestLogin(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome(ChromeDriverManager().install())
 
+    #each function begin with "test" and run alphabetically
     def test_a_success_login(self):
         # step to open browser
         browser = self.browser
         browser.get("https://opensource-demo.orangehrmlive.com")
+        browser.maximize_window()
+        time.sleep(5)
+        # step to fill in email and password
+        browser.find_element(By.NAME,"username").send_keys("Admin") 
+        browser.find_element(By.NAME,"password").send_keys("admin123") 
+        browser.find_element(By.XPATH,"//div[@id='app']/div[@class='orangehrm-login-layout']"
+            +"/div[@class='orangehrm-login-layout-blob']"
+            +"//form[@action='/web/index.php/auth/validate']/div[3]/button[@type='submit']").click()
+        time.sleep(5)
+        # assert response message
+        response_message = browser.find_element(
+            By.XPATH,"//div[@id='app']//aside[@class='oxd-sidepanel']/nav[@role='navigation']"
+            +"//ul[@class='oxd-main-menu']//a[@href='/web/index.php/admin/viewAdminModule']").text
+        self.assertEqual(response_message, 'Admin')
 
 if __name__ == "__main__": 
     unittest.main()
