e13d847326ffe67d1f2345a6b9f91304f64747fe
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-12-14 14:30:27 +0700

    Selenium Python Saucedemo - Add Unit Test to Login successfully

--- saucedemo.py
+++ saucedemo.py
@@ -16,6 +16,16 @@ class TestLogin(unittest.TestCase):
         browser = self.browser
         browser.get("https://www.saucedemo.com/")
         browser.maximize_window()
+        time.sleep(5)
+        # step to fill in email and password
+        browser.find_element(By.ID,"user-name").send_keys("standard_user") 
+        browser.find_element(By.ID,"password").send_keys("secret_sauce") 
+        browser.find_element(By.XPATH,"/html//input[@id='login-button']").click()
+        time.sleep(5)
+        # assert response message
+        response_message = browser.find_element(
+            By.CLASS_NAME,"inventory_item_name").text
+        self.assertEqual(response_message, 'Sauce Labs Backpack')
 
 if __name__ == "__main__": 
     unittest.main()
