633de173344ce33fc43bac124493044775cc6a01
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-02-16 17:27:00 +0700

    Selenium Python PythonOrg - Run the test using Webdriver Manager for Firefox

--- pythonorg.py
+++ pythonorg.py
@@ -2,11 +2,14 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
 class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        # self.driver = webdriver.Firefox()
+        self.driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
     def test_search_in_python_org(self):
         driver = self.driver
